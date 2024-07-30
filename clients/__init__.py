7112c689371510affd3580a6f9810d908847c1cd
ty8900 <ty8900@naver.com>
2024-07-30 23:15:02 +0900

    bugfix for req, init

--- clients/__init__.py
+++ clients/__init__.py
@@ -3,17 +3,13 @@ from .water_allocation_client import WaterAllocationClient
 from .public_goods_client import PublicGoodsClient
 from .movie_chat_client import MovieChatClient
 from .debate_chat_client import DebateChatClient
-from .public_goods_client_ui import PublicGoodsClientUI
-from .todo_chat_client import TodoChatClient
 
 client_list = {
     "Default": DefaultClient, 
     "WaterAllocation": WaterAllocationClient,
     "PublicGoods": PublicGoodsClient,
-    "PublicGoodsUI" : PublicGoodsClientUI,
     "MovieChat": MovieChatClient,
     "DebateChat": DebateChatClient,
-    "TodoChat" : TodoChatClient,
     }
 
 def select_client(name):
